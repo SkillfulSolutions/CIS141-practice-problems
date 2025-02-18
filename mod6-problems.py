f589452fe38000129b75e8070fb39b4f70998d0d
SkillfulSolutions <corpo.ubwpz@passfwd.com>
2025-02-18 11:18:39 -0800

    mod6 attempt2, problem 3

--- mod6-problems.py
+++ mod6-problems.py
@@ -11,9 +11,8 @@ print(count)
 #3. Given a list of strings, write code to create a new list that includes only the strings longer than three characters.
 #   Print the resulting filtered list.
 list = [ "bacon", "bed", "test", "purple", "chocolate", "rain", "pat" ]
-for s in list:
-    if len(s) > 3:
-        print(s)
+list2 = [ s for s in list if len(s) > 3 ]
+print(list2)
 
 #4. For a list of integers, write code that counts how many numbers are positive and how many are negative, then print both counts.
 list = [ -1, -3, 4, -2, -6, -5, 7, 9, -8 ]
